ecf93e866a8ef03aa3c0b0f9f3ceaa2b673195e0
Micah Wells <micahp0506@gmail.com>
2016-01-14 14:07:39 -0600

    Data storing at local storage

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,31 +13,37 @@ angular.module('starter.controllers', [])
   $scope.loginData = {};
 
   // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
+  $ionicModal.fromTemplateUrl('templates/newnote.html', {
     scope: $scope
   }).then(function(modal) {
     $scope.modal = modal;
   });
 
   // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
+  $scope.closeNote = function() {
     $scope.modal.hide();
   };
 
   // Open the login modal
-  $scope.login = function() {
+  $scope.newNote = function() {
     $scope.modal.show();
   };
 
   // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+  $scope.saveNote = function(note) {
+    console.log('note', note);
+    // Getting the history in local storage, if nothing exists, we create an empty array
+    var noteHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    // Pushes "note" information from partial into array
+    noteHistory.push(note);
+    // Setting "note" information to local storage
+    localStorage.setItem('searchHistory', JSON.stringify(noteHistory));
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
+    // $timeout(function() {
+    //   $scope.closeNote();
+    // }, 1000);
   };
 })
 
