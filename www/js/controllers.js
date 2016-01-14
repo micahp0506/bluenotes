a7d941bd8ace3f0fd61a8c6ae003b25401ddead9
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-01-14 14:38:57 -0600

    got note titles output to dom

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -48,14 +48,19 @@ angular.module('starter.controllers', [])
 })
 
 .controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    // { title: 'Reggae', id: 1 },
-    // { title: 'Chill', id: 2 },
-    // { title: 'Dubstep', id: 3 },
-    // { title: 'Indie', id: 4 },
-    // { title: 'Rap', id: 5 },
-    // { title: 'Cowbell', id: 6 }
-  ];
+
+  $scope.localHistory = JSON.parse(localStorage.getItem("searchHistory"));
+  //console.log("localHistory", localHistory);
+
+  // $scope.playlists = [
+
+  //   // { title: 'Reggae', id: 1 },
+  //   // { title: 'Chill', id: 2 },
+  //   // { title: 'Dubstep', id: 3 },
+  //   // { title: 'Indie', id: 4 },
+  //   // { title: 'Rap', id: 5 },
+  //   // { title: 'Cowbell', id: 6 }
+  // ];
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
